a5f0b5aa22a994f22fe59b064f7f17b11ff99055
PedroM-K <pedromoreiralonga999@gmail.com>
2025-09-01 10:56:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('button');
+let current = '';
 
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.getAttribute('data-value');
+
+        if(value === 'C') {
+            current = '';
+            display.value = '0';
+        } else if(value === '=') {
+            try {
+                current = eval(current);
+                display.value = current;
+            } catch {
+                display.value = 'Erro';
+                current = '';
+            }
+        } else {
+            current += value;
+            display.value = current;
+        }
+    });
+});
